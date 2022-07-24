2a50db086f9445d5b553bc5edda59c62c8cc7d0c
Alexander Reineck <areineck@ohgiftcard.com>
2022-07-24 09:47:32 -0300

    desafio10-RefactorAPI

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 import "dotenv/config.js"
 import express from 'express';
-import { Server } from 'socket.io';
 import { engine } from "express-handlebars";
 import http from 'http';
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import Contenedor from './Contenedor.js';
-import ContenedorMemoria from "./src/contenedores/ContenedorMemoria.js";
-const contenedorMemoria = new ContenedorMemoria();
-contenedorMemoria.save({name: "asd"});
 
 /* Routers */
 import { router as productosApiRouter} from './routes/productosAPI.js';
@@ -21,7 +15,6 @@ import { router as errorRouter} from './routes/error.js';
 /* Base */
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server)
 const port = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -42,38 +35,6 @@ app.use('/productos', productosRouter);
 app.use('/api/productos', productosApiRouter);
 app.use('/api/carrito', carritosApiRouter);
 app.use('/*', errorRouter);
-
-/* Variables para socketIO*/
-let mensajes = [];
-const messages_db = './messages.txt';
-const utf = 'utf-8';
-
-(async() => {
-    const contenedor = new Contenedor("messages");
-    await contenedor.initialize();
-})();
-
-/* Socket IO */
-io.on("connection", socket => {
-    console.log("SocketIO Connected!");
-    const messages = JSON.parse(fs.readFileSync(messages_db, utf));
-
-    mensajes = messages;
-
-    socket.emit("initial", messages);
-
-    socket.on("sendMessage", (data) => {
-        data.timestamp = (new Date).toLocaleString();
-        mensajes.push(data);
-        io.sockets.emit("shareMessages", mensajes);
-        fs.writeFileSync(messages_db, JSON.stringify(mensajes), utf);
-    });
-
-    socket.on("product", () => {
-        io.sockets.emit("refreshProducts");
-    })
-
-});
 
 /* Server Init */
 server.listen(port, () => {
